740f13b9891a74639c05bc185872e2b1d5c0fcad
Thiago <thiagocostas2017@gmail.com>
2020-11-17 22:04:30 -0300

    Completo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseja ver a tabuada de qual número? ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 10; contador >= 1; contador--)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+
+            }
+        }
+    }
+}
